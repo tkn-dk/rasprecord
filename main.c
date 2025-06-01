0fe8d317ee4abc3936e6d5553f970ab23bd133e2
Tage Korsdal Nielsen <tkn@korsdal.dk>
2025-06-01 12:52:52 +0200

    Visual code setup

--- main.c
+++ main.c
@@ -13,7 +13,8 @@ static void activate (GtkApplication *app, gpointer user_data)
 
   window = gtk_application_window_new (app);
   gtk_window_set_title (GTK_WINDOW (window), "Window");
-  gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+ // gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+  gtk_window_fullscreen(GTK_WINDOW(window));
 
   box = gtk_box_new (GTK_ORIENTATION_VERTICAL, 0);
   gtk_widget_set_halign (box, GTK_ALIGN_CENTER);
@@ -24,7 +25,7 @@ static void activate (GtkApplication *app, gpointer user_data)
   button = gtk_button_new_with_label ("Hello World");
 
   g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
-  g_signal_connect_swapped (button, "clicked", G_CALLBACK (gtk_window_destroy), window);
+  //g_signal_connect_swapped (button, "clicked", G_CALLBACK (gtk_window_destroy), window);
 
   gtk_box_append (GTK_BOX (box), button);
 
@@ -36,7 +37,7 @@ int main (int argc, char **argv)
   GtkApplication *app;
   int status;
 
-  app = gtk_application_new ("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
+  app = gtk_application_new ("dk.korsdal.rasprecord", G_APPLICATION_DEFAULT_FLAGS);
   g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
   status = g_application_run (G_APPLICATION (app), argc, argv);
   g_object_unref (app);
